9fc252747700b6da7c42173b82b617e98550654f
Ortender <charles.tonneau@gmail.com>
2025-08-28 16:40:18 +0200

    Update exo salaire horaire

--- Exo salaire horaire.py
+++ Exo salaire horaire.py
@@ -1,7 +1,13 @@
-def conversion_entier (a,b):
-    entier1 = int(a)
-    entier2 = int(b)
-    return entier1, entier2
+def get_int (phrase):
+    user_input_int = None
+    while not user_input_int:
+        try:
+            user_input = input(phrase)
+            user_input_int = int(user_input)
+        except:
+            phrase = "le format est incorret, veuillez saisir l'information à nouveau: "
+            continue
+    return user_input_int
 def salaire_mensuel(x):
     resultat = x/12
     return resultat
@@ -12,17 +18,10 @@ def salaire_horaire(a,b):
     resultat = a/b
     return resultat
 
-salaire_annuel = input("veuillez saisir votre salaire annuel ")
-heure_travaillées = input("veuillez saisir votre temps de travail hebdomdaire ")
-salaire_annuel_et_heures = ()
-while not salaire_annuel_et_heures :
-    try:
-        salaire_annuel_et_heures = conversion_entier(salaire_annuel, heure_travaillées)
-    except:
-        salaire_annuel = input("le salaire annuel ou les heures travaillées n'ont pas été saisis au bon format. Merci de les saisir à nouvau. Salaire annuel: ")
-        heure_travaillées = input("Heures travaillées: ")
+salaire_annuel = get_int("veuillez saisir votre salaire annuel ")
+heure_travaillées = get_int("veuillez saisir votre temps de travail hebdomdaire ")
 
-salaire_mois = salaire_mensuel(salaire_annuel_et_heures[0])
+salaire_mois = salaire_mensuel(salaire_annuel)
 salaire_semaine = salaire_hebdomadaire(salaire_mois)
-taux_horaire = salaire_horaire(salaire_semaine, salaire_annuel_et_heures[1])
+taux_horaire = salaire_horaire(salaire_semaine, heure_travaillées)
 print(f"votre salaire horaire est {round(taux_horaire, 2)}€")
